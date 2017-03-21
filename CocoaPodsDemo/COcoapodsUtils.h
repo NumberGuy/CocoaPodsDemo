8841ce21a69ed411be7605021cc3f6bb54755b6e
刚陈 <gangchen@gangchendeMacBook-Pro.local>
2017-03-21 09:50:42 +0800

    add pod files

---
+++ CocoaPodsDemo/COcoapodsUtils.h
@@ -0,0 +1,15 @@
+//
+//  COcoapodsUtils.h
+//  cocoapodsDemo
+//
+//  Created by 刚陈 on 2017/3/17.
+//  Copyright © 2017年 刚陈. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface COcoapodsUtils : NSObject
+
++ (void)pinteLog;
+
+@end
---
+++ CocoaPodsDemo/COcoapodsUtils.m
@@ -0,0 +1,18 @@
+//
+//  COcoapodsUtils.m
+//  cocoapodsDemo
+//
+//  Created by 刚陈 on 2017/3/17.
+//  Copyright © 2017年 刚陈. All rights reserved.
+//
+
+#import "COcoapodsUtils.h"
+
+@implementation COcoapodsUtils
+
++(void)pinteLog
+{
+    NSLog(@"success");
+}
+
+@end
